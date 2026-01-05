c620fed707f35395be0f73f669eef51638dcdaf1
taniya verma <vermatanya87218@gmail.com>
2026-01-05 12:11:39 +0530

    Refactor smooth scroll for anchor links

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.querySelectorAll('a').forEach(link => {
-    link.addEventListener('click', function(e) {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href'))
-        .scrollIntoView({ behavior: 'smooth' });
+            .scrollIntoView({ behavior: 'smooth' });
     });
 });
